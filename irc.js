40140e9fb1500291caf7c2065886a041c06d3083
Timoshnikov Nikita <arexthenikita@gmail.com>
2019-05-26 06:12:56 +0300

    autherication fix

--- irc.js
+++ irc.js
@@ -3,14 +3,47 @@ const token = require('./token.json');
 
 function NewClient(id, nick, password, discordClient) {
 
-    console.log('Irc:NewClient', id, nick)
+    console.log('Irc:NewClient', id, nick, token.ircserver, token.channel)
 
     var clientIrc = new irc.Client(token.ircserver, nick,{
-        channels: [token.channel],
+        autoConnect: false,
+        userName: 'IrcBridge',
+        realName: 'Test irc bridge for discord',
+        //channels: [token.channel],
+        debug: true,
+        showErrors: true,
     });
 
     clientIrc.addListener('registered', function() {
-        clientIrc.say('nickserv', 'identify ' + password);
+        console.log('REGISTERED')
+
+        // setTimeout(function(){
+        //     clientIrc.say('NickServ', 'identify ' + password);
+        // }, 2000)
+        // setTimeout(function(){
+        //     clientIrc.connect()
+
+        //     clientIrc.join(token.channel, function(msg){
+        //         console.log(msg)
+        //     })
+        // }, 5000)
+    })
+
+    clientIrc.addListener('notice', function(nick, to, text, message) {
+        if ( nick == 'NickServ' ) {
+
+            if ( text.search('This nickname is registered') >= 0 ) {
+                setTimeout(function(){
+                    console.log('SEND identify')
+                    clientIrc.say('NickServ', 'identify ' + password);
+                }, 2000)
+            } else if ( text.search('You are now identified for') >= 0  ) {
+                console.log('JOIN', token.channel)
+                clientIrc.join(token.channel, function(msg){
+                    console.log(msg)
+                })
+            }
+        }
     })
 
     clientIrc.addListener('message', function (from, to, message) {
@@ -31,7 +64,18 @@ function NewClient(id, nick, password, discordClient) {
 
     clientIrc.on('error', function(err){
         console.log(err)
+        discordClient.users.find( x => x.id === id ).send(JSON.stringify(err))
     })
+
+    clientIrc.connect(1, function(serverReply) {
+        console.log("Connected!\nserverReply=", serverReply);
+
+        
+        // clientIrc.join(token.channel, function(input) {
+        //   console.log("Joined #channel");
+          
+        // });
+    });
 
     return clientIrc
 }
